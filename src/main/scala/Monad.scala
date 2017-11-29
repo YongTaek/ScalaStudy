2f6fdd711c5f27ca023a8b9f8be7c816587ea0d9
OhYongTaek <ka123ak1@gmail.com>
2017-11-29 10:44:48 +0900

    문제 추가

--- src/main/scala/Monad.scala
+++ src/main/scala/Monad.scala
@@ -19,6 +19,7 @@ trait Functor[F[_]] {
 
 trait Monad[F[_]] extends Functor[F] {
   def unit[A](a: => A): F[A]
+
   def flatMap[A, B](ma: F[A])(f: A => F[B]): F[B]
 
   def map[A,B](ma: F[A])(f: A => B): F[B] =
@@ -26,6 +27,23 @@ trait Monad[F[_]] extends Functor[F] {
 
   def map2[A, B, C](ma: F[A], mb: F[B])(f: (A, B) => C): F[C] =
     flatMap(ma)(a => map(mb)(b => f(a, b)))
+
+  def compose[A,B,C](f: A => F[B], g: B => F[C]): A => F[C] =
+    a => flatMap(f(a))(g)
+
+  def flatMapViaCompose[A,B](ma: F[A])(f: A => F[B]): F[B] =
+    compose((_:Unit) => ma, f)(())
+
+  def join[A](mma: F[F[A]]): F[A] =
+    flatMap(mma)(identity)
+
+  def flatMapViaJoinAndMap[A,B](ma: F[A])(f: A => F[B]): F[B] =
+    join(map(ma)(f))
+
+  def compose[A,B,C](f: A => F[B], g: B => F[C]): A => F[C] =
+    a => join(map(f(a))(g))
+
+
 
   def sequence[A](lma: List[F[A]]): F[List[A]] =
     traverse(lma)(identity)
@@ -36,6 +54,47 @@ trait Monad[F[_]] extends Functor[F] {
   def replicateM[A](n: Int, ma: F[A]): F[List[A]] =
     sequence(List.fill(n)(ma))
 
+  def filterM[A](ms: List[A])(f: A => F[Boolean]): F[List[A]] =
+    ms.foldRight(unit(List.empty[A]))((a, acc) => flatMap(f(a))(x => addListIfTrue(x)(unit(a), acc)))
+
+  def addListIfTrue[A](b: Boolean)(a: F[A], list: F[List[A]]): F[List[A]] =
+    if (b) map2(a, list)(_ :: _)
+    else list
+
+  /*
+  compose(f, unit) == f
+  compose(unit, f) == f
+
+  1. compose(f, unit) == f
+  flatMap(f(a))(unit)
+  => f(a)
+  2. compose(unit, f)
+  flatMap(unit(a))(f)
+  => f(unit(a))
+  => f(a)
+   */
+
+  /*
+  compose(compose(f, g), h) == compose(f, compose(g, h))
+  a => join(map(f(a))(g))
+
+  1. compose(compose(f, g), h)
+  join(map(compose(f, g))(h))
+  => join(map(join(map(f)(g)))(h))
+  => join(join(map(f)(g)).map(h))
+  => join(join(f.map(g)).map(h))
+  => join(f.map(g)).map(h).join()
+  => f.map(g).join().map(h).join()
+
+  2. compose(f, compose(g, h))
+  join(map(f)(compose(g, h)))
+  => join(map(f)(join(map(g)(h))))
+  => join(f.map(join(map(g)(h))))
+  => join(f.map(join(g.map(h))))
+  => f.map(join(g.map(h))).join()
+  => f.map(g.map(h).join()).join()
+
+   */
 
 
 }
@@ -74,10 +133,6 @@ object Monad {
     override def flatMap[A, B](ma: Stream[A])(f: A => Stream[B]) = ma.flatMap(f)
   }
 
-  case class State[S, A](run: S => (A, S)) {
-    def flatMap[B](f: A => State[S, B]): Monad.State[S, B] = ???
-
-  }
 //
 //  val stateMonad = new Monad[({type l[k] = State[Int, k]})#l] {
 //    override def unit[A](a: => A) = State(s => (a, s))
@@ -100,8 +155,52 @@ object Monad {
   List(List(1, 1), List(1, 2), List(1, 3), List(2, 1), List(2, 2), List(2, 3), List(3, 1), List(3, 2), List(3, 3))이 나온다.
    */
 
-  
+  /*
+  Par 에서 결합법칙의 의미.
+  Par에서 flatMap이 가지는 의미는 첫 번째 인수의 결과를 가지고 두 번쨰 인수에 대해 적용시키는 것이다. 즉 3개의 thread를 결합하는 상황에 있다면 어짜피 안쪽부터 순서대로 실행될 것이기 때문에
+  1,2를 결합한 뒤 3을 결합하는 것이나 2,3을 결합한뒤 1과 결합하는 것이나 같은 의미라는 것이다.
+   */
 
+  case class Id[A](value: A) {
+    def map[B](f: A => B):Id[B] = Id(f(value))
+
+    def flatMap[B](f: (A) => Id[B]) = f(value)
+  }
+
+  val IdMonad = new Monad[Id] {
+    override def unit[A](a: => A) = Id(a)
+
+    override def flatMap[A, B](ma: Id[A])(f: (A) => Id[B]) = ma flatMap f
+  }
+
+  case class State[S, A](run: S => (A, S)) {
+    def map[B](f: A => B): State[S, B] =
+      State(s => {
+        val (a, s1) = run(s)
+        (f(a), s1)
+      })
+    def flatMap[B](f: A => State[S, B]): State[S, B] =
+      State(s => {
+        val (a, s1) = run(s)
+        f(a).run(s1)
+      })
+  }
+
+  type IntState[A] = State[Int, A]
+
+  object IntStateMonad extends Monad[IntState] {
+    override def unit[A](a: => A): IntState[A] = State(s => (a, s))
+
+    override def flatMap[A, B](ma: IntState[A])(f: (A) => IntState[B]): IntState[B] = ma flatMap f
+  }
+
+  def stateMonad[S] = new Monad[({type f[x] = State[S, x]})#f] {
+    override def unit[A](a: => A) = State(s => (a, s))
+
+    override def flatMap[A, B](ma: State[S, A])(f: (A) => State[S, B]) = ma flatMap f
+  }
+
+  
 
 }
 
@@ -110,4 +209,13 @@ object Main extends App {
 
   println(OptionMonad.replicateM(3, Some(1)))
   println(ListMonad.replicateM(2, List(1,2,3)))
+
+  // 11.11
+  def F:Int => Option[Int] =
+    (a:Int) => Some(a + 3)
+
+  println(OptionMonad.compose(F, (a:Int) => OptionMonad.unit(a))(1))
+  println(F(1))
+
+
 }
